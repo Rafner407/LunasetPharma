06882e1da9a5d73cfa7b103c67e3bc2f77402830
RafnerGabriel <75384376+RafnerGabriel@users.noreply.github.com>
2022-10-19 11:06:18 -0300

    produtos

--- cart.php
+++ cart.php
@@ -14,7 +14,7 @@ if(isset($_POST['update_cart'])){
    $cart_id = $_POST['cart_id'];
    $cart_quantity = $_POST['cart_quantity'];
    mysqli_query($conn, "UPDATE `cart` SET quantity = '$cart_quantity' WHERE id = '$cart_id'") or die('query failed');
-   $message[] = 'cart quantity updated!';
+   $message[] = 'quantidade atualizada!';
 }
 
 if(isset($_GET['delete'])){
@@ -66,16 +66,16 @@ if(isset($_GET['delete_all'])){
             while($fetch_cart = mysqli_fetch_assoc($select_cart)){   
       ?>
       <div class="box">
-         <a href="cart.php?delete=<?php echo $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a>
-         <img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" alt="">
+         <a href="cart.php?delete=<?php echo $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('deletar este item do carrinho?');"></a>
+         <img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" alt="" style="height: 250px; width: 250px;">
          <div class="name"><?php echo $fetch_cart['name']; ?></div>
-         <div class="price">$<?php echo $fetch_cart['price']; ?>/-</div>
+         <div class="price">$<?php echo $fetch_cart['price']; ?></div>
          <form action="" method="post">
             <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
             <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
-            <input type="submit" name="update_cart" value="update" class="option-btn">
+            <input type="submit" name="update_cart" value="atualizar" class="option-btn">
          </form>
-         <div class="sub-total"> sub total : <span>$<?php echo $sub_total = ($fetch_cart['quantity'] * $fetch_cart['price']); ?>/-</span> </div>
+         <div class="sub-total"> sub total : <span>$<?php echo $sub_total = ($fetch_cart['quantity'] * $fetch_cart['price']); ?></span> </div>
       </div>
       <?php
       $grand_total += $sub_total;
@@ -87,11 +87,11 @@ if(isset($_GET['delete_all'])){
    </div>
 
    <div style="margin-top: 2rem; text-align:center;">
-      <a href="cart.php?delete_all" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('delete all from cart?');">delete all</a>
+      <a href="cart.php?delete_all" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('delete all from cart?');">deletar tudo</a>
    </div>
 
    <div class="cart-total">
-      <p>total : <span>$<?php echo $grand_total; ?>/-</span></p>
+      <p>total : <span>$<?php echo $grand_total; ?></span></p>
       <div class="flex">
          <a href="shop.php" class="option-btn">continue comprando</a>
          <a href="checkout.php" class="btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">avance para revisar</a>
