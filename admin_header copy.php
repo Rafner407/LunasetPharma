8457cc6bd3d79bcb085481391c9498b14890e9e9
RafnerGabriel <75384376+RafnerGabriel@users.noreply.github.com>
2022-10-19 16:56:52 -0300

    copy

--- admin_header copy.php
+++ admin_header copy.php
@@ -10,14 +10,6 @@ if(isset($message)){
    }
 }
 ?>
-<head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-<link rel="stylesheet" href="css/sideBar.css">
-<script src="js/sideBar.js"></script>
-</head>
 
 <body>
 <header class="header" id="header">
@@ -25,14 +17,6 @@ if(isset($message)){
 
 
    <div class="flex" id="body-pd">
-   <div class="l-navbar" id="nav-bar">
-        <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
-                <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> </div>
-            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-        </nav>
-    </div>
-      <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
       <a href="admin_page.php" class="logo"><span>Lunaset</span> painel do admin</a>
       <nav class="navbar">
          <a href="admin_page.php">início</a>
